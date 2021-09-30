20001a220896935f1e0316ce6625cffe0d919b5f
Bartek Świderski <bartekzg@gmail.com>
2021-09-30 18:49:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import Notiflix from "../node_modules/notiflix";
 const qs = (selector) => document.querySelector(selector);
 const qsa = (selector) => document.querySelectorAll(selector);
 const countryList = qs(".country-list")
+const countryInfo = qs(".country-info")
 const form = qs("#search-box");
-let del = qsa(".add");
 let markup = ""
 const DEBOUNCE_DELAY = 300;
 let arr = [];
@@ -13,7 +13,6 @@ function fetchUsers() {
   let countryName = form.value.trim();
   return fetch(`https://restcountries.com/v2/name/${countryName}?fields=name,capital,population,flag,languages`)
   .then(response => {
-    console.log(response)
     if (!response.ok) {
       Notiflix.Notify.failure("Oops, there is no country with that name");
     }
@@ -26,9 +25,8 @@ function fetchUsers() {
   .catch(error => {
    });
 }
-
 form.addEventListener("input", debounce(
-  () => {del.forEach((it)=>it.remove());
+  () =>{
     fetchUsers()
       .then((users) => renderCountry(users))
       .catch((error) => console.log(error));
@@ -37,20 +35,45 @@ form.addEventListener("input", debounce(
 
 function renderCountry(arr) {
   console.log(arr)
+  const divCounter= qsa(".add")
+  for (let j = 0; j<divCounter.length; j++){ 
+    const rem = qs(".add")
+    rem.remove()}
   if (arr.length>10){Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")};
-  if (arr.length === 0){Notiflix.Notify.failure("Oops, there is no country with that name")}; 
+  if (arr.status === 404){Notiflix.Notify.failure("Oops, there is no country with that name")}; 
   if (arr.length>1 && arr.length<=10){
     let flag = arr.map(coun => coun.flag)
     let name = arr.map(coun => coun.name)
     for( let i =0; i<arr.length; i++){
-      markup+=`<li class="add">
+      markup+=`<li class="add" style="display:flex; list-style: none;align-items: center">
       <img 
       src=${flag[i]}
       alt="flag"
-      width="50"
+      height="24"
+      width="30"
       >
-      <p>${name[i]}</p></img>
+      <p style="padding-left:10px">${name[i]}</p></img>
         </li>`}
         countryList.insertAdjacentHTML("beforeend", markup);
-        markup  = "";
-}}
+        markup = ""
+  }
+  if (arr.length === 1){
+    let flag = arr.map(country => country.flag)
+    let name = arr.map(country => country.name)
+    let capital = arr.map(country => country.capital)
+    let population = arr.map(country => country.population)
+    let lang=[...arr]
+    .flatMap(country=>country.languages)
+    .map(l=>l.name)
+    .join(", ")
+   markup =`<ul class="add" style=" list-style: none;align-items: center">
+   <div style="display:flex ;align-items: center"><img src=${flag} alt="flag" height="40" width="50"><h2 style="padding-left:20px">${name}</h2></img></div>
+   <li style="display:flex;align-items: center"><b>Capital:</b><p style="padding-left:10px">${capital}</p></li>
+   <li style="display:flex;align-items: center"><b>Population:</b><p style="padding-left:10px">${population}</p></li>
+   <li style="display:flex;align-items: center"><b>Languages:</b><p style="padding-left:10px">${lang}</p></li>
+     </ul>`;
+
+     countryInfo.insertAdjacentHTML("beforeend", markup);
+     markup = ""
+}
+}
